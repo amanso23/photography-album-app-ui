41a862c240790d39d6d0d0b940fae1f761c2d73c
Adrian <adrianmanso80@gmail.com>
2025-03-04 11:55:03 +0000

    Add TypeScript interfaces for Album, Image, User, and UserPlan

---
+++ src/types.d.ts
@@ -0,0 +1,57 @@
+import type { backgrounds } from "./lib/backgrounds";
+
+export interface Album {
+    id: number;
+    tag: Tag;
+    title: string;
+    cover: string;
+    artist: string;
+    background: string;
+    images: Image[]
+  }
+
+  export interface Image {
+    id: number;
+    title: string;
+    artist: string;
+    cover: string;
+  }
+
+  export type Tag = 
+  | 'Urban'
+  | 'Abstract'
+  | 'Nature'
+  | 'Futuristic'
+  | 'Anime'
+  | 'Vintage'
+  | 'Black & White'
+  | 'Minimalist'
+  | 'Landscape'
+  | 'Cosmic'
+  | 'Portraits'
+  | 'Street Photography'
+  | 'Architecture'
+  | 'Still Life'
+  | 'Conceptual'
+  | 'Wildlife'
+  | 'Artistic'
+  | 'Retro'
+  | 'Surreal'
+  | 'Scenic'
+  | 'Candid'
+  | 'Night Photography'
+  | 'Underwater'
+  | 'Macro'
+  | 'Fantasy';
+
+
+export interface User {
+  avatar: string;
+  name: string;
+  plan: UserPlan
+}
+
+export type UserPlan = 
+  | "Premium"
+  | "Extra"
+  | "Essential"
