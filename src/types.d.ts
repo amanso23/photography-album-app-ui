d2d00c36341dde230891966ebd1b95a446ef2afd
Adrian <adrianmanso80@gmail.com>
2025-03-06 19:27:17 +0000

    Remove Tag type from Album interface to simplify type definitions

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,6 @@ import type { backgrounds } from "./lib/backgrounds";
 
 export interface Album {
     id: number;
-    tag: Tag;
     title: string;
     cover: string;
     artist: string;
@@ -17,32 +16,7 @@ export interface Album {
     cover: string;
   }
 
-  export type Tag = 
-  | 'Urban'
-  | 'Abstract'
-  | 'Nature'
-  | 'Futuristic'
-  | 'Anime'
-  | 'Vintage'
-  | 'Black & White'
-  | 'Minimalist'
-  | 'Landscape'
-  | 'Cosmic'
-  | 'Portraits'
-  | 'Street Photography'
-  | 'Architecture'
-  | 'Still Life'
-  | 'Conceptual'
-  | 'Wildlife'
-  | 'Artistic'
-  | 'Retro'
-  | 'Surreal'
-  | 'Scenic'
-  | 'Candid'
-  | 'Night Photography'
-  | 'Underwater'
-  | 'Macro'
-  | 'Fantasy';
+
 
 
 export interface User {
