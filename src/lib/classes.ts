7693baca6586cdef8e1c60fa81a95c992d797985
Adrian <adrianmanso80@gmail.com>
2025-03-09 11:25:47 +0000

    Refactor album component styles for improved layout and add source link functionality

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -1,16 +1,17 @@
 export const classes = {
-    album: {
-        side: {
-            topClass: 'flex gap-x-[1.5vh] min-w-[35vh] md:w-auto', //top class
-            h1: 'font-medium text-[2.5vh] md:text-[3vh] lg:text-[2.3vh]', //title
-            h3: 'font-light text-[2.2vh] md:text-[2.5vh] lg:text-[2vh] opacity-70 ', //artist
-            image: 'size-[7vh] md:size-[8.5vh] lg:size-[6.5vh]', //cover
-        }, //album style for component Side.astro
-        home: { 
-            topClass: '',
-            h1: 'text-[3.5vh] font-bold leading-none',
-            h3: 'text-[2.5vh]',
-            image: 'w-full h-auto aspect-square transition-all duration-200 group-hover:scale-110'
-        } //album style for page Home.astro
-    }
-}
+  album: {
+    side: {
+      topClass: "flex gap-x-[1.5vh] min-w-[35vh] md:w-auto", //top class
+      h1: "font-medium text-[2.5vh] md:text-[3vh] lg:text-[2.3vh]", //title
+      h3: "font-light text-[2.2vh] md:text-[2.5vh] lg:text-[2vh] opacity-70 ", //artist
+      image: "size-[7vh] md:size-[8.5vh] lg:size-[6.5vh]", //cover
+    }, //album style for component Side.astro
+    home: {
+      topClass: "",
+      h1: "text-[3.5vh] font-bold leading-none",
+      h3: "text-[2.5vh]",
+      image:
+        "w-full h-auto aspect-square transition-all duration-200 group-hover:scale-110",
+    }, //album style for page Home.astro
+  },
+};
